583c4ab343a785e68565ada6aed429907f48f368
Chase Oliphant <chase@bravo6.io>
2026-02-09 11:43:48 -0800

    re-usable perl script to simplify word .rtf

---
+++ simplify_rtf.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Getopt::Long qw(GetOptions);
+
+my $in = '';
+my $out = '';
+GetOptions(
+  'in=s'  => \$in,
+  'out=s' => \$out,
+) or die "Usage: $0 --in INPUT.rtf --out OUTPUT.rtf\n";
+
+if (!$in || !$out) {
+  die "Usage: $0 --in INPUT.rtf --out OUTPUT.rtf\n";
+}
+
+open my $fh, '<', $in or die "Failed to open input: $in\n";
+local $/;
+my $s = <$fh>;
+close $fh;
+
+$s =~ s/\r\n/\n/g;
+
+sub rtf_unicode {
+  my ($n) = @_;
+  $n += 65536 if $n < 0;
+  return '"' if $n == 8220 || $n == 8221;
+  return "'" if $n == 8216 || $n == 8217;
+  return '-' if $n == 8211 || $n == 8212;
+  return chr($n) if $n >= 0 && $n < 128;
+  return '';
+}
+
+open my $outfh, '>', $out or die "Failed to open output: $out\n";
+
+print $outfh "{\\rtf1\\ansi\\ansicpg1252\\deff0\n";
+print $outfh "{\\fonttbl{\\f0 Roboto;}}\n";
+print $outfh "\\pard\\plain\\f0\\fs24\n";
+
+while ($s =~ /\\listtext.*?([0-9]+\.|[a-z]\.)\\tab\}?(.+?)(?=\\par(?![a-z]))/sig) {
+  my ($label, $body) = ($1, $2);
+  $body =~ s/\\u(-?\d+)\??/rtf_unicode($1)/ge;
+  $body =~ s/\\\'([0-9a-fA-F]{2})/chr(hex($1))/ge;
+  $body =~ s/[\n\r\t]+//g;
+  $body =~ s/\\[a-zA-Z]+-?\d* ?//g;
+  $body =~ s/\\[^a-zA-Z]//g;
+  $body =~ s/[{}]//g;
+  $body =~ s/[ ]{2,}/ /g;
+  $body =~ s/^\s+|\s+$//g;
+  next unless length $body;
+
+  my $prefix = ($label =~ /^[a-z]/i) ? "\\tab " : "";
+  my $text = $label . " " . $body;
+  $text =~ s/([\\{}])/\\$1/g;
+  $text =~ s/[^\x00-\x7F]//g;
+  print $outfh $prefix, $text, "\\par\n";
+}
+
+print $outfh "}\n";
+close $outfh;
